04f3eff566efed4ebd35270cfa16e94d6f1240ce
Altay Atalay <altayatalay@protonmail.com>
2022-05-14 22:39:14 +0300

    minor change to ui

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
-import {Box} from '@mui/material'
+import {Box, Link} from '@mui/material'
 
 import GitHubIcon from '@mui/icons-material/GitHub';
 
@@ -24,17 +24,25 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
 	  <Box display='flex' justifyContent='flex-end' paddingRight='10px' paddingTop='10px'>
-		<a href='https://github.com/comp306-project'  target="_blank" rel="noreferrer">
+		<Link href='https://github.com/comp306-project'  target="_blank" rel="noreferrer" underline='none'>
 			<Box
 				display= 'inline-flex'
 				flexWrap= 'wrap'
 				alignItems= 'center'
 				color= {theme.palette.text.primary}
+				fontWeight= 'Medium'
+				sx={[
+					{
+						'&:hover': {
+							color: theme.palette.text.secondary,
+						},
+					},
+				]}
 			>
 				<GitHubIcon fontSize='small'/>
 				<Box margin='5px' fontSize='18px'>Github</Box>
 			</Box>
-		</a>
+		</Link>
 	  </Box>
     </ThemeProvider>
   );
